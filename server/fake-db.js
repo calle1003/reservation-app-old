fc58fb3eb6b613267cc0a02cf80b0860397f8a95
calle1003 <akimoto.takao9603@gmail.com>
2025-11-09 22:57:46 +0900

    Nodeサーバー構築

---
+++ server/fake-db.js
@@ -0,0 +1,89 @@
+const Product = require('./model/product');
+
+class FakeDB {
+  constructor() {
+    this.products = [
+      // ← ★「product」→「products」に修正
+      {
+        coverImage: './assets/img/phone-cover.jpg',
+        name: 'Phone XL',
+        price: 799,
+        description: 'A large phone with one of the best screens',
+        heading1: 'サンプルテキスト1',
+        heading2: 'サンプルテキスト2',
+        heading3: 'サンプルテキスト3',
+        headingtext1:
+          'サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト',
+        headingtext2:
+          'テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章',
+        headingtext3:
+          'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+      },
+      {
+        coverImage: './assets/img/phone-cover.jpg',
+        name: 'Phone Mini',
+        price: 699,
+        description: 'A great phone with one of the best cameras',
+        heading1: 'サンプルテキスト1',
+        heading2: 'サンプルテキスト2',
+        heading3: 'サンプルテキスト3',
+        headingtext1:
+          'サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト',
+        headingtext2:
+          'テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章',
+        headingtext3:
+          'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+      },
+      {
+        coverImage: './assets/img/phone-cover.jpg',
+        name: 'Phone Standard',
+        price: 299,
+        description: 'A great phone with one of the best cameras',
+        heading1: 'サンプルテキスト1',
+        heading2: 'サンプルテキスト2',
+        heading3: 'サンプルテキスト3',
+        headingtext1:
+          'サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト',
+        headingtext2:
+          'テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章',
+        headingtext3:
+          'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+      },
+      {
+        coverImage: './assets/img/phone-cover.jpg',
+        name: 'Phone ProMax',
+        price: 999,
+        description: 'A great phone with one of the best cameras',
+        heading1: 'サンプルテキスト1',
+        heading2: 'サンプルテキスト2',
+        heading3: 'サンプルテキスト3',
+        headingtext1:
+          'サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト',
+        headingtext2:
+          'テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章',
+        headingtext3:
+          'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+      },
+      // ほかのデータも同様
+    ];
+  }
+
+  async pushProductsToDb() {
+    try {
+      await Product.deleteMany({});
+      for (let product of this.products) {
+        const newProduct = new Product(product);
+        await newProduct.save();
+      }
+      console.log('✅ データをDBに挿入しました');
+    } catch (err) {
+      console.error('❌ データ挿入中にエラー:', err);
+    }
+  }
+
+  seeDb() {
+    this.pushProductsToDb();
+  }
+}
+
+module.exports = FakeDB;
